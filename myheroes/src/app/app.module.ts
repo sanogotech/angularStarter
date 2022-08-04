43740b4032d831d85b20f3af3f5f89fa4c287c92
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2022-08-04 16:24:21 +0000

    update

--- myheroes/src/app/app.module.ts
+++ myheroes/src/app/app.module.ts
@@ -14,6 +14,12 @@ import { MatSidenavModule} from  '@angular/material/sidenav';
 import {MatListModule} from  '@angular/material/list';
 import {MatButtonModule} from  '@angular/material/button';
 import {MatIconModule} from  '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio'; 
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
@@ -32,7 +38,8 @@ import { HomeComponent } from './components/home/home.component';
     MatListModule,
     MatButtonModule,
     MatIconModule,
-    MatDividerModule
+    MatDividerModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
